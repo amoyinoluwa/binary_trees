127708503b30d850dc24eb5b196c47494d2b6a4c
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-11-15 11:56:33 -0800

    --

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,20 @@
+/**
+ * binary_tree_insert_left -  inserts node as left child of parent
+ * @parent - parent node
+ * @value: value that left child holds
+ * Return: ptr to new node
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *left_child;
+
+	left_child = binary_tree_node(parent, value);
+
+	if (!left_child)
+		return (NULL);
+
+	parent->left = left_child;
+
+	return (left_child);
+}
